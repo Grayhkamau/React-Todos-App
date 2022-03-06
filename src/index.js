e26b0e1c3e3649360ca94534934ba9df72ef756b
Humphrey Kamau <hkamau263@gmail.com>
2022-03-06 22:17:51 +0300

    ''

--- src/index.js
+++ src/index.js
@@ -4,20 +4,16 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 import Todocontainer from './components/todocontainer';
 import {ContextClass} from './todoContext';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <>
     <ContextClass>
-    <HashRouter >
+    <BrowserRouter basename={process.env.PUBLIC_URL} >
     <Todocontainer />
-    </HashRouter>
+    </BrowserRouter>
     </ContextClass>
   </>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
